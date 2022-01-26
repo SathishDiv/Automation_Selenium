18c521b6f433631d5653c636d86005e84b306fb2
sathishsuriya <sathishmd.mailme@gmail.com>
2022-01-26 19:03:04 +0530

    Good Version

---
+++ SI_Bliss_Project/src/FirstTest.java
@@ -0,0 +1,13 @@
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("ArtificalIntelligence");
+		System.out.println("Datascience");
+		System.out.println("cloud compiyting");
+	}
+
+}
